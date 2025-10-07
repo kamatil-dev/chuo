1c999c57cc96d409cbd6e648afce5e202d4b517d
kamatil <karim.amahtil@gmail.com>
2025-10-07 18:07:54 +0100

    Remove unused global middleware for route navigation

--- middleware/global.ts
+++
@@ -1,5 +0,0 @@
-export default defineNuxtRouteMiddleware((from) => {
-	const user = useState<User>("user")
-	if ((from.name === 'database-admin-tables' || from.name === 'database-admin') && user.value.service)
-		return navigateTo(`/admin/tables/${user.value.service}`)
-})
