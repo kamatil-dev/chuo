aa570c40bc0053657bad2b5d36d9d707cd2f65a6
KAMATIL <Karim.amahtil@gmail.com>
2025-10-09 10:14:19 +0100

    Update API base URL in app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,5 @@
 export default defineAppConfig({
-	apiBase: "https://chuo.inicontent/api/",
+	apiBase: "https://chuo.inicontent.com/api/",
 	idOne: "d7b3d61a582e53ee29b5a1d02a436d55",
 	database: "chuo",
 	fileBase: "https://files.inicontent.com/",
